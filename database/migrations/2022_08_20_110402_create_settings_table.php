3852707b7bd5f7a42cc574bbf60f3841a9984892
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 14:09:42 +0300

    Create settings migration table

---
+++ database/migrations/2022_08_20_110402_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('strkey', 32);
+            $table->mediumText('strval_en');
+            $table->mediumText('strval_ar');
+            $table->integer('created_by');
+            $table->string('title', 120);
+            $table->tinyInteger('notrans');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
