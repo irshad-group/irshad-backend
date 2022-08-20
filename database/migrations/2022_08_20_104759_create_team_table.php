b3a326c3b3d324e114e58334beb7b0bb1ac77a15
TahseenAlaa <raspoteen7@gmail.com>
2022-08-21 02:10:23 +0300

    Add kurdish fields to team migration table

--- database/migrations/2022_08_20_104759_create_team_table.php
+++ database/migrations/2022_08_20_104759_create_team_table.php
@@ -17,14 +17,17 @@ return new class extends Migration
             $table->id();
             $table->string('title_en', 128);
             $table->string('title_ar', 128);
+            $table->string('title_kr', 128);
             $table->string('thumb_img', 128);
             $table->string('img', 128);
             $table->integer('v_order')->comment('vertical order'); // vertical order
             $table->string('job_title_en', 128)->nullable();
             $table->string('job_title_ar', 128)->nullable();
+            $table->string('job_title_kr', 128)->nullable();
             $table->string('email', 128)->nullable();
             $table->string('bio_en', 200)->nullable();
             $table->string('bio_ar', 200)->nullable();
+            $table->string('bio_kr', 200)->nullable();
             $table->tinyInteger('location');
             $table->tinyInteger('enabled');
             $table->timestamps();
