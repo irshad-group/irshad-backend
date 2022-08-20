3e42eec1c4f92e6c1192361c7c8fcbb21ce2c6bc
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 14:41:02 +0300

    Create procedure items migration table

---
+++ database/migrations/2022_08_20_112218_create_proc_items_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proc_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('proc_id');
+            $table->string('title_en', 220);
+            $table->string('title_ar', 220);
+            $table->string('title_kr', 220)->nullable();
+            $table->longText('descr_en');
+            $table->longText('descr_ar');
+            $table->longText('descr_kr')->nullable();
+            $table->tinyInteger('enabled');
+            $table->string('url');
+            $table->timestamp('publish_date')->nullable();
+            $table->timestamp('last_update');
+            $table->integer('last_update_by');
+            $table->integer('created_by');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proc_items');
+    }
+};
