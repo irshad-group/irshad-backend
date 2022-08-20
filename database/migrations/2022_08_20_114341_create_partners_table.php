1d173d25792f570489a55bfd20b6b3502364b997
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 14:46:28 +0300

    Create partners migration table

---
+++ database/migrations/2022_08_20_114341_create_partners_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_en', 64);
+            $table->string('title_ar', 64);
+            $table->string('logo', 200);
+            $table->string('url', 200);
+            $table->integer('v_order')->comment('vertical order'); // vertical order
+            $table->integer('location');
+            $table->integer('created_by');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+};
