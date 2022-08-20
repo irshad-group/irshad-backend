05278501a662ba60f0e81fca83de06780cf534df
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 16:52:07 +0300

    Create directoraties_prov migration table

---
+++ database/migrations/2022_08_20_134916_create_directoraties_prov_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directoraties_prov', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_ar', 300);
+            $table->string('title_en', 300);
+            $table->string('title_kr', 300);
+            $table->integer('province_id');
+            $table->string('gps_lat');
+            $table->string('gps_lon');
+            $table->string('address');
+            $table->integer('created_by_id');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('directoraties_prov');
+    }
+};
