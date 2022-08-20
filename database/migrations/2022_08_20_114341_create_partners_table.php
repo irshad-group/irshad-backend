8088646bf3aa31e773cbe18cc6c5ac84de4f95ce
TahseenAlaa <raspoteen7@gmail.com>
2022-08-21 02:07:47 +0300

    Add kurdish title to partners migration table

--- database/migrations/2022_08_20_114341_create_partners_table.php
+++ database/migrations/2022_08_20_114341_create_partners_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title_en', 64);
             $table->string('title_ar', 64);
+            $table->string('title_kr', 64);
             $table->string('logo', 200);
             $table->string('url', 200);
             $table->integer('v_order')->comment('vertical order'); // vertical order
