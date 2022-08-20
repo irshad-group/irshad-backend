d4146e515c41b91e087cc2c86f4b8034318867af
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 13:58:37 +0300

    Create team migration table

---
+++ database/migrations/2022_08_20_104759_create_team_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team', function (Blueprint $table) {
+            $table->id();
+            $table->string('title_en', 128);
+            $table->string('title_ar', 128);
+            $table->string('thumb_img', 128);
+            $table->string('img', 128);
+            $table->integer('v_order')->comment('vertical order'); // vertical order
+            $table->string('job_title_en', 128)->nullable();
+            $table->string('job_title_ar', 128)->nullable();
+            $table->string('email', 128)->nullable();
+            $table->string('bio_en', 200)->nullable();
+            $table->string('bio_ar', 200)->nullable();
+            $table->tinyInteger('location');
+            $table->tinyInteger('enabled');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team');
+    }
+};
