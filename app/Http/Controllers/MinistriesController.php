f669a99788d90c87fac5bb847e8525b4516b9cb0
TahseenAlaa <raspoteen7@gmail.com>
2022-08-21 02:23:41 +0300

    Create ministries model and controller

---
+++ app/Http/Controllers/MinistriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ministries;
+use Illuminate\Http\Request;
+
+class MinistriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ministries  $ministries
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ministries $ministries)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ministries  $ministries
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ministries $ministries)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ministries  $ministries
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ministries $ministries)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ministries  $ministries
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ministries $ministries)
+    {
+        //
+    }
+}
