e2f68f38fbc0deac6bbfafe6097cb248fe708c0d
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 16:29:03 +0300

    Create faq migration table

--- database/migrations/2022_08_20_132513_create_faq_table.php
+++ database/migrations/2022_08_20_132513_create_faq_table.php
@@ -15,7 +15,16 @@ return new class extends Migration
     {
         Schema::create('faq', function (Blueprint $table) {
             $table->id();
+            $table->string('title_en', 220);
+            $table->string('title_ar', 220);
+            $table->string('title_kr', 300);
+            $table->text('descr_en');
+            $table->text('descr_ar');
+            $table->text('descr_kr');
+            $table->integer('v_order')->comment('vertical order'); // vertical order
+            $table->integer('created_by_id');
             $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
         });
     }
 
