c735ed1c864497f1cfed8bb6dda87a9361ce0484
TahseenAlaa <raspoteen7@gmail.com>
2022-08-20 17:09:40 +0300

    Refactor users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,26 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('username');
+            $table->string('full_name');
+            $table->integer('profile_id')->comment('0: End users, 1: Moderators, 2: Admins'); //0: End users, 1: Moderators, 2: Admins
+            $table->string('profile_pic');
+            $table->string('job_title');
+            $table->string('last_login');
+            $table->string('last_login_ip');
+            $table->text('token');
+            $table->string('salt');
+            $table->string('fb_token');
+            $table->string('fb_id');
+            $table->string('google_token');
+            $table->integer('created_by_id'); // User Id
             $table->timestamps();
+            $table->softDeletes('deleted_at', 0); //DateTime with timezone
         });
     }
 
