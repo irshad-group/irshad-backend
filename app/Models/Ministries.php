817c0e64816c4520694f71aa03b680e21c695de2
TahseenAlaa <raspoteen7@gmail.com>
2022-08-21 03:07:54 +0300

    Fix columns name and add table name to model of ministries

--- app/Models/Ministries.php
+++ app/Models/Ministries.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ministries extends Model
 {
-    use HasFactory;
+    protected $table = 'ministries';
+
+    use HasFactory, SoftDeletes;
 }
